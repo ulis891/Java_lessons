9ba96e1234d79d40d79af8b3101851a6bf68a1d8
alrksandrkostromin <ulis89@yandex.ru>
2023-09-07 00:55:15 +0300

    upd HW10

--- Java/src/S10/HW/Box.java
+++ Java/src/S10/HW/Box.java
@@ -3,7 +3,54 @@ package S10.HW;
 import java.util.ArrayList;
 
 public class Box<T extends Fruit> {
-    private ArrayList<T> fructs;
+    protected ArrayList<T> fruits;
+
+    private int size;
+
+    public Box(ArrayList<T> fruits) {
+        this.fruits = fruits;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Box{" + " weight=" + getWeight() +
+                '}';
+    }
+
+
+
+    protected float getWeight(){
+        if (fruits == null)
+            return 0;
+        else {
+            return fruits.size() * fruits.get(0).getWeight();
+        }
+    }
+
+
+
+    protected void add(ArrayList<T> fruits){
+        this.fruits = fruits;
+    }
+
+    protected boolean compare(Box box){
+        if (this.getWeight() == box.getWeight()) {
+            return true;
+        }
+        else return false;
+    }
+
+//    protected void relocate(Box<T> boxOut){
+//        if (T  instanceof boxOut.fruits ) {
+//            // todo доделать
+//        }
+//        else {
+//            System.out.println("Нельзя смешивать фрукты!");
+//        }
+//    }
+
 }
 
 
--- Java/src/S10/HW/Program.java
+++ Java/src/S10/HW/Program.java
@@ -1,7 +1,35 @@
 package S10.HW;
 
+import java.util.ArrayList;
+
 public class Program {
     public static void main(String[] args) {
+
+        ArrayList<Apple> apples = new ArrayList<>();
+        Apple apple1 = new Apple();
+        Apple apple2 = new Apple();
+        Apple apple3 = new Apple();
+        apples.add(apple1);
+        apples.add(apple2);
+        apples.add(apple3);
+
+        ArrayList<Orange> oranges = new ArrayList<>();
+        Orange orange1 = new Orange();
+        Orange orange2 = new Orange();
+        Orange orange3 = new Orange();
+        oranges.add(orange1);
+        oranges.add(orange2);
+//        oranges.add(orange3);
+
+        Box<Apple> appleBox = new Box<>(apples);
+        Box<Orange> orangeBox = new Box<>(oranges);
+
+
+        System.out.println(orangeBox);
+        System.out.println(appleBox);
+        System.out.println(appleBox.getWeight());
+        System.out.println(orangeBox.getWeight());
+        System.out.println(orangeBox.compare(appleBox));
 
     }
 }
