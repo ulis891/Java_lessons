fac45ad5d6eae0b0d97bfe5438271a10f33339f0
alrksandrkostromin <ulis89@yandex.ru>
2023-09-07 16:27:01 +0300

    upd HW10

--- Java/src/S10/HW/Box.java
+++ Java/src/S10/HW/Box.java
@@ -3,25 +3,27 @@ package S10.HW;
 import java.util.ArrayList;
 
 public class Box<T extends Fruit> {
+
     protected ArrayList<T> fruits;
 
-    private int size;
 
-    public Box(ArrayList<T> fruits) {
-        this.fruits = fruits;
+    public Box() {
+this.fruits = new ArrayList<T>();
     }
-
 
 
     @Override
     public String toString() {
-        return "Box{" + " weight=" + getWeight() +
-                '}';
+        if (fruits.isEmpty()) {
+            return "Ящик пустой";
+
+        }
+        return String.valueOf(this.fruits.get(0).getClass()).split("\\.")[2] +
+                "Box" + " весом = " + getWeight() + " кг.";
     }
 
 
-
-    protected float getWeight(){
+    protected float getWeight() {
         if (fruits == null)
             return 0;
         else {
@@ -30,27 +32,38 @@ public class Box<T extends Fruit> {
     }
 
 
-
-    protected void add(ArrayList<T> fruits){
+    protected void add(ArrayList<T> fruits) {
         this.fruits = fruits;
     }
 
-    protected boolean compare(Box box){
-        if (this.getWeight() == box.getWeight()) {
-            return true;
-        }
-        else return false;
+    protected void add2(T fruit) {
+        this.fruits.add(fruit);
     }
 
-//    protected void relocate(Box<T> boxOut){
-//        if (T  instanceof boxOut.fruits ) {
-//            // todo доделать
-//        }
-//        else {
-//            System.out.println("Нельзя смешивать фрукты!");
-//        }
-//    }
+    protected boolean compare(Box box) {
+        if (this.getWeight() == box.getWeight()) {
+            System.out.printf("Вес обоих ящиков %.2f кг.\n", this.getWeight());
+            return true;
+        } else {
+            System.out.println("Вес ящиков разный.");
+            return false;
+        }
+    }
 
+
+    protected void relocate(Box boxOut) {
+        if (this.fruits.get(0).getClass().equals(boxOut.fruits.get(0).getClass())) {
+            this.fruits.addAll(boxOut.fruits);
+            boxOut.fruits = new ArrayList<T>();
+            String frt = String.valueOf(this.fruits.get(0).getClass()).split("\\.")[2];
+            System.out.printf("Теперь в ящике %s, весом %.2f кг.\n", frt, this.getWeight());
+        } else {
+            System.out.println("Нельзя смешивать фрукты!");
+
+        }
+    }
 }
+
+
 
 
--- Java/src/S10/HW/Program.java
+++ Java/src/S10/HW/Program.java
@@ -5,31 +5,47 @@ import java.util.ArrayList;
 public class Program {
     public static void main(String[] args) {
 
-        ArrayList<Apple> apples = new ArrayList<>();
+        Box<Apple> appleBox1 = new Box<>();
+        Box<Apple> appleBox2 = new Box<>();
+        Box<Orange> orangeBox = new Box<>();
+
         Apple apple1 = new Apple();
         Apple apple2 = new Apple();
         Apple apple3 = new Apple();
-        apples.add(apple1);
-        apples.add(apple2);
-        apples.add(apple3);
+        appleBox1.add2(apple1);
+        appleBox1.add2(apple2);
+        appleBox1.add2(apple3);
 
-        ArrayList<Orange> oranges = new ArrayList<>();
+        Apple apple4 = new Apple();
+        Apple apple5 = new Apple();
+        Apple apple6 = new Apple();
+        Apple apple7 = new Apple();
+        Apple apple8 = new Apple();
+        appleBox2.add2(apple4);
+        appleBox2.add2(apple5);
+        appleBox2.add2(apple6);
+        appleBox2.add2(apple7);
+        appleBox2.add2(apple8);
+
         Orange orange1 = new Orange();
         Orange orange2 = new Orange();
-        Orange orange3 = new Orange();
-        oranges.add(orange1);
-        oranges.add(orange2);
-//        oranges.add(orange3);
-
-        Box<Apple> appleBox = new Box<>(apples);
-        Box<Orange> orangeBox = new Box<>(oranges);
+        orangeBox.add2(orange1);
+        orangeBox.add2(orange2);
 
 
         System.out.println(orangeBox);
-        System.out.println(appleBox);
-        System.out.println(appleBox.getWeight());
-        System.out.println(orangeBox.getWeight());
-        System.out.println(orangeBox.compare(appleBox));
+        System.out.println(appleBox1);
+        System.out.println(appleBox2);
+
+        System.out.println(orangeBox.compare(appleBox1));
+        System.out.println(appleBox1.compare(appleBox2));
+
+        appleBox1.relocate(appleBox2);
+        System.out.println(appleBox1);
+        System.out.println(appleBox2);
+
+        appleBox1.relocate(orangeBox);
+
 
     }
 }
